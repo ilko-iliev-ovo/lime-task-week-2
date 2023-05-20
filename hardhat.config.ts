f9c7ddb77e689afbfc5b496d6e4736a1a9c7bb53
ilko-iliev-ovo <ilko.iliev@ovo.com>
2023-05-21 00:59:07 +0300

    Added initial tests

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import secrets from './secrets.json'
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: secrets.INFURA_SEPOLIA_URL,
+      accounts: [secrets.PRIVATE_KEY],
+    }
+  },
 };
 
 export default config;
--- test/usElection.ts
+++ test/usElection.ts
@@ -55,6 +55,57 @@ describe("USElection", function () {
     expect(await usElection.currentLeader()).to.equal(2); // TRUMP
   });
 
+  // Tests from Ilko Iliev
+  it("Should throw when external sender tries to submit results", async function () {
+    const [owner, addr1] = await ethers.getSigners();
+    const stateResults = ["California", 1000, 900, 32];
+
+    await expect(usElection.connect(addr1).submitStateResult(stateResults)).to.be.revertedWith(
+        "Not invoked by the owner"
+    );
+  });
+
+  it("Should throw when both results are equal for a state", async function () {
+    const stateResults = ["California", 1000, 1000, 32];
+
+    await expect(usElection.submitStateResult(stateResults)).to.be.revertedWith(
+        "There cannot be a tie"
+    );
+  });
+
+  it("Should throw when both results are equal for a state", async function () {
+    const stateResults = ["California", 1000, 900, 0];
+
+    await expect(usElection.submitStateResult(stateResults)).to.be.revertedWith(
+        "States must have at least 1 seat"
+    );
+  });
+
+  it("Should throw when both results are equal for a state", async function () {
+    const stateResults = ["California", 1000, 900, 0];
+
+    await expect(usElection.submitStateResult(stateResults)).to.be.revertedWith(
+        "States must have at least 1 seat"
+    );
+  });
+
+  it("Should get 0 as result", async function () {
+
+    const stateResults1 = ["Tenessy", 900, 1000, 32];
+    const stateResults2 = ["Texas", 1000, 900, 32];
+
+    console.log(usElection.resultsSubmitted)
+
+    let submitTx1 = await usElection.submitStateResult(stateResults1);
+    await submitTx1.wait();
+
+    let submitTx2 = await usElection.submitStateResult(stateResults2);
+    await submitTx2.wait();
+
+    expect(await usElection.currentLeader()).to.equal(0);
+    // Tie result
+  });
+
   it("Should end the elections, get the leader and election status", async function () {
     const endElectionTx = await usElection.endElection();
 
@@ -62,8 +113,15 @@ describe("USElection", function () {
 
     expect(await usElection.currentLeader()).to.equal(2); // TRUMP
 
-    expect(await usElection.electionEnded()).to.equal(true); // Ended
+    expect(await usElection.electionEnded()).to.equal(true)
+        .and.to.emit(usElection, "LogElectionEnded"); // Ended
   });
 
-  //TODO: ADD YOUR TESTS
+  it("Throw when trying to submit results to an ended election", async function () {
+    const stateResults = ["California", 1000, 900, 0];
+
+    await expect(usElection.submitStateResult(stateResults)).to.be.revertedWith(
+        "The election has ended already"
+    );
+  });
 });
